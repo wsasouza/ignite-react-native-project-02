1f569ad87026ae91461b3b913530fd8a7a103442
wsasouza <wsasouza@hotmail.com>
2021-11-17 14:20:47 -0300

    Feat: Create Resume Screen and History Card

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { HistoryCard } from '../../Components/HistoryCard';
+
+import { 
+  Container,
+  Header,
+  Title
+} from './styles';
+
+export function Resume() {
+  return(
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard 
+        title="Compras"
+        amount="R$ 1.200,00"
+        color="red"
+      />
+
+    </Container>
+  );
+}
+
+
+
